9aae30f4dbd403a88dc9ea3e4c4f66034cb36e7e
Rover <752963883@qq.com>
2019-11-26 21:12:07 +0800

    a try

---
+++ myFirstJava.java
@@ -0,0 +1,11 @@
+/*
+ * @Author: Rover
+ * @Date: 2019-11-26 21:04:06
+ * @LastEditors: Rover
+ * @LastEditTime: 2019-11-26 21:06:10
+ */
+class myFirstJava {
+    public static void main(String[] args) {
+        System.out.println("I am Rover");    
+    }
+}
