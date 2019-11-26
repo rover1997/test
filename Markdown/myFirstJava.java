b8b0d8e2c06e53ff786f605624df2f221206371d
Rover King <752963883@qq.com>
2019-11-26 22:08:22 +0800

    another test

---
+++ Markdown/myFirstJava.java
@@ -0,0 +1,11 @@
+/*
+ * @Author: Rover
+ * @Date: 2019-11-25 17:31:45
+ * @LastEditors: Rover
+ * @LastEditTime: 2019-11-25 17:34:06
+ */
+class myFirstJava{
+    public static void main(String[] args) {
+        System.out.println("hello world");   
+    }
+}
---
+++ Markdown/myFirstPython.py
@@ -0,0 +1,7 @@
+'''
+@Author: Rover
+@Date: 2019-11-25 17:29:37
+@LastEditors: Rover
+@LastEditTime: 2019-11-25 17:30:25
+'''
+print('hello world')
